6ba60350fa7c863e1aadf764308f9842b1d69f9d
Biplob Sutradhar <biplobsd11@gmail.com>
2023-04-23 23:34:54 +0600

    v1.1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const STORIES_URL = ["https://elearn.daffodilvarsity.edu.bd/"];
-export const DEFAULT_STATUS_MSG = 'Ready for request';
-export const APP_NAME = 'BLC auto login';
-export const VERSION = 'v1.0';
-export const REPO_URL = 'https://github.com/biplobsd/blc-auto-login.git';
+export const DEFAULT_STATUS_MSG = "Ready for request";
+export const APP_NAME = "BLC auto login";
+export const VERSION = "v1.1";
+export const REPO_URL = "https://github.com/biplobsd/blc-auto-login.git";
